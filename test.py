34c4b5b5a0bcdae332235c8570e9d12387d08c0a
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-10-19 03:57:26 +0530

    All three type of coursed included

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ import json
 from bs4 import BeautifulSoup
 
 dep = input("Enter department code in 2 letters. e.g. MA/CS/GG ")
-filename = dep+'.csv'
+course = input("Enter 1 for B.tech , 2 for Int Msc., 3 for Dualdegree ")
+if(course=='1' or course=='2'):
+    filename = dep+'.csv'
+else:
+    filename = dep+'-Dual'+'.csv'
 f=open(filename,'w')
-for yr in range(15,10,-1):
+for yr in range(15,9,-1):
     for stu in range(1,61):
         j = str(stu).rjust(2, '0')
         k = str(yr)
-        rollno = k+dep+"200"+j
+        rollno = k+dep+course+"00"+j
         url = "https://erp.iitkgp.ernet.in/StudentPerformance/view_performance.jsp?rollno="+rollno
         content = requests.get(url)
         p =open('pointer','w')

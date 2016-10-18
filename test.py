aa46e3f2551fe29e683e224c9b8309587bfba393
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-10-18 22:31:39 +0530

    Script made

--- test.py
+++ test.py
@@ -3,30 +3,36 @@ import json
 from bs4 import BeautifulSoup
 
 dep = input("Enter department code in 2 letters. e.g. MA/CS/GG ")
-for i in range(1,20):
-    j = str(i).rjust(2, '0')
-    rollno = "15"+dep+"200"+j
-    url = "https://erp.iitkgp.ernet.in/StudentPerformance/view_performance.jsp?rollno="+rollno
-    filename = dep+'.txt'
-    f=open(filename,'w')
-    content = requests.get(url)
-    soup = BeautifulSoup(content.text, 'lxml')
+filename = dep+'.csv'
+f=open(filename,'w')
+for yr in range(15,10,-1):
+    for stu in range(1,61):
+        j = str(stu).rjust(2, '0')
+        k = str(yr)
+        rollno = k+dep+"200"+j
+        url = "https://erp.iitkgp.ernet.in/StudentPerformance/view_performance.jsp?rollno="+rollno
+        content = requests.get(url)
+        soup = BeautifulSoup(content.text, 'lxml')
 
-    sgpa = soup.find_all("b",string = "SGPA")
-    cgpa = soup.find_all("b",string = " CGPA")
-    sgpa.reverse()
-    cgpa.reverse()
-    f.write('cgpa sgpa \n')
-#print(cgpa)
-    for i,j in zip(sgpa,cgpa):
-        i = i.find_parent("td")
-        i = i.find_next_sibling("td")
-        i = i.string
-        j = j.find_parent("td")
-        j = j.find_next_sibling("td")
-        j = j.string
-        print(i+' '+j+',')
-        f.write(i+' '+j+',')
-    f.write('\n') 
+        sgpa = soup.find_all("b",string = "SGPA")
+        cgpa = soup.find_all("b",string = " CGPA")
+        sgpa.reverse()
+        cgpa.reverse()
+        #print(cgpa)
+        for i,j in zip(sgpa,cgpa):
+            i = i.find_parent("td")
+            i = i.find_next_sibling("td")
+            i = i.string
+            j = j.find_parent("td")
+            j = j.find_next_sibling("td")
+            j = j.string
+            #print(type(i))
+            #print(i+' '+j+',')
+            f.write(i+' '+j+',')
+            #f.write('done,')
+        f.write('\n') 
+        if(stu%5==0):
+            print(stu,'done')
+    print(yr,' year done')
 f.close()
 print('Done!')
